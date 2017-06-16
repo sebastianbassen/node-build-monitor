2a11f2f2980e59e50a5c25326e642ccdc6b0e968
Sebastian Bassen <sebastian.bassen@bornholdtlee.de>
2017-06-16 14:49:30 +0200

    fixed mergeerror

--- app/services/GitLab.js
+++ app/services/GitLab.js
@@ -1,11 +1,11 @@
 var request = require('request'),
-  async = require('async');
+async = require('async');
 
 var now = function () {
-    return (new Date()).getTime();
-  },
-  second = 1000,
-  minute = second * 60;
+  return (new Date()).getTime();
+},
+second = 1000,
+minute = second * 60;
 
 module.exports = function () {
   var self = this;
@@ -55,13 +55,13 @@ module.exports = function () {
 
   function getProjectsApiUrl(page, per_page) {
     var base = self.config.url + '/',
-      query = '?page=' + page + '&per_page=' + per_page;
+    query = '?page=' + page + '&per_page=' + per_page;
     return base + 'api/v3/projects' + query;
   }
 
   function getProjectBuildsApiUrl(project, page, per_page) {
     var base = self.config.url + '/',
-      query = '?page=' + page + '&per_page=' + per_page;
+    query = '?page=' + page + '&per_page=' + per_page;
     return base + 'api/v3/projects/' + project.id + '/builds' + query;
   }
 
@@ -109,15 +109,15 @@ module.exports = function () {
   function getBuildStatus(project, build) {
     switch (build.status) {
       case 'pending':
-        return '#ffa500';
+      return '#ffa500';
       case 'running':
-        return 'Blue';
+      return 'Blue';
       case 'failed':
-        return 'Red';
+      return 'Red';
       case 'success':
-        return 'Green';
+      return 'Green';
       default:
-        return 'Gray';
+      return 'Gray';
     }
   }
 
@@ -191,80 +191,80 @@ module.exports = function () {
       if (!err && response.statusCode == 200) {
         var urls = [], pages = Math.ceil(
           parseInt(response.headers['x-total-pages'], 10));
-        for (var i = 2; i <= pages; i = i + 1) {
-          urls.push(getPagedApiUrl(i, 100));
-        }
+          for (var i = 2; i <= pages; i = i + 1) {
+            urls.push(getPagedApiUrl(i, 100));
+          }
 
-        process.nextTick(function () {
-          callback(body);
-        });
-
-        async.mapSeries(urls, function (url, pass) {
-          log('Fetching', url);
-          request({
-            headers: getRequestHeaders(),
-            url: url,
-            json: true
-          }, function (err, response, body) {
-            if (!err && response.statusCode == 200) {
-              process.nextTick(function () {
-                callback(body);
-              });
-              process.nextTick(function () {
-                pass(null, body);
-              });
-            } else {
-              log('Error', body);
-              process.nextTick(function () {
-                callback([]);
-              });
-              process.nextTick(function () {
-                pass(null, []);
-              });
-            }
-          });
-        });
-      } else {
-        log('Error', body);
-        process.nextTick(function () {
-          callback([]);
-        });
-      }
-    });
-  }
-
-  function updateBuild(project, build, callback) {
-    log('Fetching', getBuildApiUrl(project, build));
-    request({
-      headers: getRequestHeaders(),
-      url: getBuildApiUrl(project, build),
-      json: true
-    }, function (err, response, body) {
-      if (!err && response.statusCode == 200) {
-        body.monitor = getBuildMonitorBuild(project, body);
-        body.expires = getBuildExpiration(body);
-        project.builds[body.monitor.id] = body;
-        if (typeof callback === 'function') {
           process.nextTick(function () {
             callback(body);
           });
-        }
-      } else {
-        log('Error', body);
-        if (typeof callback === 'function') {
+
+          async.mapSeries(urls, function (url, pass) {
+            log('Fetching', url);
+            request({
+              headers: getRequestHeaders(),
+              url: url,
+              json: true
+            }, function (err, response, body) {
+              if (!err && response.statusCode == 200) {
+                process.nextTick(function () {
+                  callback(body);
+                });
+                process.nextTick(function () {
+                  pass(null, body);
+                });
+              } else {
+                log('Error', body);
+                process.nextTick(function () {
+                  callback([]);
+                });
+                process.nextTick(function () {
+                  pass(null, []);
+                });
+              }
+            });
+          });
+        } else {
+          log('Error', body);
           process.nextTick(function () {
-            callback(build);
+            callback([]);
           });
         }
-      }
-    });
-  }
+      });
+    }
 
-  function reduceBuilds(builds, callback) {
-    const seen = {};
-    let latest = null;
+    function updateBuild(project, build, callback) {
+      log('Fetching', getBuildApiUrl(project, build));
+      request({
+        headers: getRequestHeaders(),
+        url: getBuildApiUrl(project, build),
+        json: true
+      }, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+          body.monitor = getBuildMonitorBuild(project, body);
+          body.expires = getBuildExpiration(body);
+          project.builds[body.monitor.id] = body;
+          if (typeof callback === 'function') {
+            process.nextTick(function () {
+              callback(body);
+            });
+          }
+        } else {
+          log('Error', body);
+          if (typeof callback === 'function') {
+            process.nextTick(function () {
+              callback(build);
+            });
+          }
+        }
+      });
+    }
 
-    results = builds
+    function reduceBuilds(builds, callback) {
+      const seen = {};
+      let latest = null;
+
+      results = builds
       .filter(build => {
         const key = build.monitor.id;
         if (typeof seen[key] === 'undefined') {
@@ -287,100 +287,107 @@ module.exports = function () {
         }
       });
 
-    if (typeof callback === 'function') {
-      process.nextTick(function () {
-        callback(results);
-      });
+      if (typeof callback === 'function') {
+        process.nextTick(function () {
+          callback(results);
+        });
+      }
     }
-  }
 
-  function fetchProjectBuilds(project, callback) {
-    requestFirstPage(function (page, per_page) {
-      return getProjectBuildsApiUrl(project, page, per_page);
-    }, function (results) {
-      results.forEach((build, index) => {
-        results.find(item => item.id === build.id).monitor = getBuildMonitorBuild(project, build);
-        results.find(item => item.id === build.id).expires = getBuildExpiration(build);
-      });
+    function fetchProjectBuilds(project, callback) {
+      requestFirstPage(function (page, per_page) {
+        return getProjectBuildsApiUrl(project, page, per_page);
+      }, function (results) {
+        results.forEach((build, index) => {
+          results.find(item => item.id === build.id).monitor = getBuildMonitorBuild(project, build);
+          results.find(item => item.id === build.id).expires = getBuildExpiration(build);
+        });
 
-      process.nextTick(function () {
-        reduceBuilds(results, function (results) {
-          if (results.length) {
-            log(project.name_with_namespace + ' | ' +
+        process.nextTick(function () {
+          reduceBuilds(results, function (results) {
+            if (results.length) {
+              log(project.name_with_namespace + ' | ' +
               results.length + ' current builds.');
-          }
-          process.nextTick(function () {
-            callback(results);
+            }
+            process.nextTick(function () {
+              callback(results);
+            });
           });
         });
       });
-    });
-  }
+    }
 
     function updateProject(project, callback) {
-        log('Updating project:', project.name_with_namespace);
-        if (self.config.slugs.indexOf('*/*') > -1 || self.config.slugs.indexOf(project.namespace.name + "/*")  > -1 || self.config.slugs.indexOf(project.path_with_namespace) > -1) {
-          if (typeof project.builds === 'undefined') {
-              project.builds = {};
-          }
-          if (Object.keys(builds).length) {
-            project.builds = builds;
-          }
+      log('Updating project:', project.name_with_namespace);
+      if (self.config.slugs.indexOf('*/*') > -1 || self.config.slugs.indexOf(project.namespace.name + "/*")  > -1 || self.config.slugs.indexOf(project.path_with_namespace) > -1) {
+        if (typeof project.builds === 'undefined') {
+          project.builds = {};
+        }
+        if (project.builds_enabled === true) {
+          fetchProjectBuilds(project, function(results) {
+            var i, build, builds = {};
+            for (i = 0; i < results.length; i = i + 1) {
+              build = results[i];
+              builds[build.monitor.id] = build;
+            }
+            if (Object.keys(builds).length) {
+              project.builds = builds;
+            }
+            project.expires = getProjectExpiration(project);
+            self.cache.projects[project.id] = project;
+            if (typeof callback === 'function') {
+              process.nextTick(function() {
+                callback(project);
+              });
+            }
+          });
+        } else {
+          project.builds = {};
           project.expires = getProjectExpiration(project);
           self.cache.projects[project.id] = project;
           if (typeof callback === 'function') {
-            process.nextTick(function () {
+            process.nextTick(function() {
               callback(project);
             });
           }
-        });
+        }
       } else {
-        project.builds = {};
-        project.expires = getProjectExpiration(project);
-        self.cache.projects[project.id] = project;
         if (typeof callback === 'function') {
-          process.nextTick(function () {
+          process.nextTick(function() {
             callback(project);
           });
         }
       }
-    } else {
-      if (typeof callback === 'function') {
-        process.nextTick(function () {
-          callback(project);
-        });
-      }
     }
-  }
 
-  function fetchNewProjects(callback) {
-    self.cache.expires = getDefaultExpiration();
+    function fetchNewProjects(callback) {
+      self.cache.expires = getDefaultExpiration();
 
-    log('Fetching new projects...');
-    requestAllPages(getProjectsApiUrl, function (projects) {
-      projects
+      log('Fetching new projects...');
+      requestAllPages(getProjectsApiUrl, function (projects) {
+        projects
         .filter(project => project.builds_enabled)
         .forEach(project => {
           updateProject(project);
         });
 
-      log('Found', projects.length + ' new projects.');
-      if (typeof callback === 'function') {
-        process.nextTick(function () {
-          callback(projects);
-        });
-      }
-    });
-  }
-
-  self.check = function (callback) {
-    // Trigger fetch for new projects
-    if (now() > self.cache.expires) {
-      process.nextTick(fetchNewProjects);
+        log('Found', projects.length + ' new projects.');
+        if (typeof callback === 'function') {
+          process.nextTick(function () {
+            callback(projects);
+          });
+        }
+      });
     }
 
-    // Iterate through already cached projects
-    async.mapSeries(Object.keys(self.cache.projects),
+    self.check = function (callback) {
+      // Trigger fetch for new projects
+      if (now() > self.cache.expires) {
+        process.nextTick(fetchNewProjects);
+      }
+
+      // Iterate through already cached projects
+      async.mapSeries(Object.keys(self.cache.projects),
       function (key, pass) {
         var project = self.cache.projects[key];
 
@@ -393,27 +400,27 @@ module.exports = function () {
 
         // Iterate through already cached builds for the project
         async.mapSeries(Object.keys(project.builds),
-          function (key, pass) {
-            var build = project.builds[key];
+        function (key, pass) {
+          var build = project.builds[key];
 
-            // Trigger fetch for build with expired cache
-            if (now() > build.expires) {
-              process.nextTick(function () {
-                updateBuild(project, build);
-              });
-            }
-
-            // Pass along the monitor version of the build info
+          // Trigger fetch for build with expired cache
+          if (now() > build.expires) {
             process.nextTick(function () {
-              pass(null, build.monitor);
+              updateBuild(project, build);
             });
-          }, function (err, results) {
+          }
 
-            // Pass along all project builds
-            process.nextTick(function () {
-              pass(null, results);
-            });
+          // Pass along the monitor version of the build info
+          process.nextTick(function () {
+            pass(null, build.monitor);
           });
+        }, function (err, results) {
+
+          // Pass along all project builds
+          process.nextTick(function () {
+            pass(null, results);
+          });
+        });
       }, function (err, builds) {
 
         // Reduce builds from all projects into a flat array
@@ -427,39 +434,39 @@ module.exports = function () {
           });
         });
       });
-  };
+    };
 
-  self.configure = function (config) {
-    self.config = config;
-    if (typeof self.config.intervals === 'undefined') {
-      self.config.intervals = {};
-    }
-    if (typeof self.config.intervals.disabled === 'undefined') {
-      self.config.intervals.disabled = 12 * 60 * minute;
-    }
-    if (typeof self.config.intervals.empty === 'undefined') {
-      self.config.intervals.empty = 10 * minute;
-    }
-    if (typeof self.config.intervals.default === 'undefined') {
-      self.config.intervals.default = minute;
-    }
-    if (typeof self.config.slugs === 'undefined') {
-      self.config.slugs = ['*/*'];
-    }
-    if (typeof process.env.GITLAB_TOKEN !== 'undefined') {
-      self.config.token = process.env.GITLAB_TOKEN;
-    }
-    if (typeof self.config.caPath !== 'undefined') {
-      request = request.defaults({
-        agentOptions: {
-          ca: require('fs').readFileSync(self.config.caPath).toString().split("\n\n")
-        }
-      });
-    }
-    for (var key in self.config) {
-      if (key !== 'token') {
-        log(key + ':', self.config[key]);
+    self.configure = function (config) {
+      self.config = config;
+      if (typeof self.config.intervals === 'undefined') {
+        self.config.intervals = {};
       }
-    }
+      if (typeof self.config.intervals.disabled === 'undefined') {
+        self.config.intervals.disabled = 12 * 60 * minute;
+      }
+      if (typeof self.config.intervals.empty === 'undefined') {
+        self.config.intervals.empty = 10 * minute;
+      }
+      if (typeof self.config.intervals.default === 'undefined') {
+        self.config.intervals.default = minute;
+      }
+      if (typeof self.config.slugs === 'undefined') {
+        self.config.slugs = ['*/*'];
+      }
+      if (typeof process.env.GITLAB_TOKEN !== 'undefined') {
+        self.config.token = process.env.GITLAB_TOKEN;
+      }
+      if (typeof self.config.caPath !== 'undefined') {
+        request = request.defaults({
+          agentOptions: {
+            ca: require('fs').readFileSync(self.config.caPath).toString().split("\n\n")
+          }
+        });
+      }
+      for (var key in self.config) {
+        if (key !== 'token') {
+          log(key + ':', self.config[key]);
+        }
+      }
+    };
   };
-};
